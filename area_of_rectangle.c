c699d9c5318e184be2c5d7906723f57bf1858c52
Monika Kharmale <156334972+MonikaKharmale162003@users.noreply.github.com>
2025-02-05 09:55:56 +0530

    Add files via upload

---
+++ area_of_rectangle.c
@@ -0,0 +1,14 @@
+// AREA OF RECTANGLE
+
+#include<stdio.h>
+int main(){
+int length;
+int width;
+int area;
+printf("enter the length:");
+scanf("%d",&length);
+printf("enter the width:");
+scanf("%d",&width);
+area=length*width;
+printf("area of rectangle=%d",area);
+}
