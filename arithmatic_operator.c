1966f4e815ed135eda2b8a1364dc89618f9c7e56
Monika Kharmale <156334972+MonikaKharmale162003@users.noreply.github.com>
2025-02-05 09:56:35 +0530

    Add files via upload

---
+++ arithmatic_operator.c
@@ -0,0 +1,16 @@
+//ARITHMATIC OPERATOR
+
+# include<stdio.h>
+   int main(){
+    int a=10;
+    int b=20;
+    int sum=a+b;
+    int mul=a*b;
+    int div1=a/b;
+    int div2=a%b;
+    printf("sum=%d\n",sum);
+    printf("mul=%d\n",mul);
+    printf("div1=%d\n",div1);
+    printf("div2=%d\n",div2);
+}
+
