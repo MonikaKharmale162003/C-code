e6f86ec3a7fa1863b0cb9896e6acef2d700dea21
Monika Kharmale <156334972+MonikaKharmale162003@users.noreply.github.com>
2025-02-05 22:30:20 +0530

    Add files via upload

---
+++ largest_number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a=30;
+    int b=140;
+    int c=80;
+    if(a>b && a>c)
+{
+printf("30 is largest");
+}
+else if(b>a && b>c){
+    printf("140 is largest");
+}
+else{
+    printf("80 is lrgest");
+}
+}
