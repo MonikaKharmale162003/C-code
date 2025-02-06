1594db749379412a352cbaaacbc493b533142935
Monika Kharmale <156334972+MonikaKharmale162003@users.noreply.github.com>
2025-02-06 21:44:02 +0530

    Add files via upload

---
+++ print_no_loop.c
@@ -0,0 +1,34 @@
+//FOR LOOP
+
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=10;i++){
+        printf("%d\n",i);
+    }
+    return 0;
+}
+
+//WHILE LOOP
+ 
+ #include<stdio.h>
+ int main(){
+    int i=1;
+   while(i<=10){
+     printf("%d\n",i);
+    i++;
+   }
+ }
+ 
+//DO WHILE LOOP
+
+
+#include<stdio.h>
+int main()
+{ 
+    int i=1;
+    do{
+        printf("%d\n",i);
+        i++;
+    }
+    while(i<=10);
+}
