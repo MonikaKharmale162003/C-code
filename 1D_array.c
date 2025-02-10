621b22f6e6c76dc92f217334d4d6fa40ee7a7b29
Monika Kharmale <156334972+MonikaKharmale162003@users.noreply.github.com>
2025-02-10 22:54:16 +0530

    Add files via upload

---
+++ 1D_array.c
@@ -0,0 +1,23 @@
+//1 DIMENTION ARRAY
+
+#include<stdio.h>
+int main(){
+int num[3];
+
+    num[0]=10;
+    num[1]=20;
+    num[2]=30;
+
+    printf("%d ",num[0]);
+    printf("%d ",num[1]);
+    printf("%d ",num[2]);
+
+    // for(int i=0;i<=2;i++){
+    //     printf("%d ",num[i]);
+    // }
+
+}
+
+
+
+
